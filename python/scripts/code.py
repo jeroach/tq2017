7fc943ac307becb8e1a3a22bd304848ea77b4779
Josh Roach <josh.roach@D2L.com>
2017-07-13 11:20:25 -0400

    Adding merge_2_lists python example

--- python/scripts/code.py
+++ python/scripts/code.py
@@ -2,5 +2,12 @@ def return_5():
     return 5
 
 
+def merge_2_lists(listA, listB):
+    mergedList = []
+    mergedList.extend(a for a in listA if a not in mergedList)
+    mergedList.extend(b for b in listB if b not in mergedList)
+    return mergedList
+
+
 if __name__ == "__main__":
     return_5()
--- python/tests/code_test.py
+++ python/tests/code_test.py
@@ -5,3 +5,14 @@ from scripts import code
 def test_return_5():
     expected_result = code.return_5()
     assert 5 == expected_result
+
+
+@pytest.mark.parametrize('listA,listB,expected_result', [
+  (['A', 'B'], ['C', 'D'], ['A', 'B', 'C', 'D']),
+  (['A', 'A'], ['A', 'B'], ['A', 'B']),
+  ([], ['C'], ['C']),
+  ([], [], [])
+])
+def test_merge_2_lists(listA, listB, expected_result):
+    result = code.merge_2_lists(listA, listB)
+    assert result == expected_result
