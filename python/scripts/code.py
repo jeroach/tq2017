7f26d40ef5c3ab0b98862e45f558922cdad94e08
Josh Roach <josh.roach@D2L.com>
2017-07-13 11:55:04 -0400

    Add mock example for Python

--- python/scripts/code.py
+++ python/scripts/code.py
@@ -1,3 +1,6 @@
+import os
+
+
 def return_5():
     return 5
 
@@ -7,6 +10,14 @@ def merge_2_lists(listA, listB):
     mergedList.extend(a for a in listA if a not in mergedList)
     mergedList.extend(b for b in listB if b not in mergedList)
     return mergedList
+
+
+def get_filenames_in_directory(directory):
+    fileNames = []
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            fileNames.append(name)
+    return fileNames
 
 
 if __name__ == "__main__":
--- python/tests/code_test.py
+++ python/tests/code_test.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest import mock
 from scripts import code
 
 
@@ -16,3 +17,14 @@ def test_return_5():
 def test_merge_2_lists(listA, listB, expected_result):
     result = code.merge_2_lists(listA, listB)
     assert result == expected_result
+
+
+@mock.patch('os.walk')
+def test_get_filenames_in_directory(mockwalk):
+    folders = ['folder1', 'folder2']
+    files = ['a.txt', 'b.txt', 'c.png']
+    mockwalk.return_value = [('/root', folders, files)]
+
+    result = code.get_filenames_in_directory('path')
+    assert len(result) == 3
+    assert result == files
