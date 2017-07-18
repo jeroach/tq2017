ec88744205809e90b0fbfe3c650f1dd10ebaaafb
Josh Roach <josh.roach@D2L.com>
2017-07-18 13:48:26 -0400

    Added another test and renamed methods to be more clear of behaviour demonstrated

--- python/tests/code_test.py
+++ python/tests/code_test.py
@@ -3,7 +3,7 @@ from unittest import mock
 from scripts import code
 
 
-def test_return_5():
+def test_simple_assert():
     expected_result = code.return_5()
     assert 5 == expected_result
 
@@ -14,17 +14,33 @@ def test_return_5():
   ([], ['C'], ['C']),
   ([], [], [])
 ])
-def test_merge_2_lists(listA, listB, expected_result):
+def test_with_cases(listA, listB, expected_result):
     result = code.merge_2_lists(listA, listB)
     assert result == expected_result
 
 
 @mock.patch('os.walk')
-def test_get_filenames_in_directory(mockwalk):
+def test_with_mocked_logic(mockwalk):
     folders = ['folder1', 'folder2']
     files = ['a.txt', 'b.txt', 'c.png']
     mockwalk.return_value = [('/root', folders, files)]
 
     result = code.get_filenames_in_directory('path')
     assert len(result) == 3
+    assert result == files
+
+
+@mock.patch('os.walk')
+@pytest.mark.parametrize('files,expected_count', [
+  (['a.txt', 'b.txt', 'c.png'], 3),
+  (['a.txt', 'c.png'], 2),
+  ([], 0),
+  (['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 10)
+])
+def test_with_mocked_logic_and_test_cases(mockwalk, files, expected_count):
+    folders = ['folder1', 'folder2']
+    mockwalk.return_value = [('/root', folders, files)]
+
+    result = code.get_filenames_in_directory('path')
+    assert len(result) == expected_count
     assert result == files
