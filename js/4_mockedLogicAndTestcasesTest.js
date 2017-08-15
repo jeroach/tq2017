4626dd4d36e3b0dd9a22ee1ae67438b620c59cbe
Josh Roach <josh.roach@D2L.com>
2017-08-15 13:59:57 -0400

    Adding another file of JS tests

---
+++ js/4_mockedLogicAndTestcasesTest.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const sinon = require('sinon');
+const assert = require('assert');
+
+function getFilenamesInDirectory(directory) {
+    let fileNames = [];
+    fs.readdir(directory, (err, files) => {
+        files.forEach(file => {
+            fileNames.push(file);
+        });
+    });
+    return fileNames;
+}
+
+describe('Test that testcases work in parallel with stubbed logic', function () {
+    const tests = [
+        {files: ['a.txt', 'b.txt', 'c.png']},
+        {files: ['a.txt', 'c.png']},
+        {files: []},
+        {files: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']}
+    ];
+
+    tests.forEach(function(test) {
+        it('stub should return list of files containing ' + test.files, function () {
+            let fsStub = sinon.stub(fs, 'readdir').callsFake(function(path, callback) {
+                callback(null, test.files);
+            });
+
+            const result = getFilenamesInDirectory('path');
+            assert.equal(result.length, test.files.length);
+            assert.deepEqual(result, test.files); // structual equality (checks that operands are equivalent)
+
+            fsStub.restore();
+        });
+    });
+});
