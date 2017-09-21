e6f35b0f105efff2173cc48e21e4e95805cff1a2
Josh Roach <josh.roach@D2L.com>
2017-09-21 11:46:31 -0400

    Improved technique - addfinalizer and parameterize

--- python/5_setup_and_teardown_test.py
+++ python/5_setup_and_teardown_test.py
@@ -1,35 +1,32 @@
+# Tests are isolated
+
 import os
 import pytest
 
 
-@pytest.fixture
-def setup_file():
+@pytest.fixture(scope='class')
+def setup_file(request):
     f = open('test_file.txt', 'a+')
     f.write('Some text')
     f.seek(0)  # needed for Windows specific bug
 
-    yield f
+    def teardown():
+        print('Closing and deleting file')
+        f.close()
+        os.remove('test_file.txt')
+    request.addfinalizer(teardown)
+    return f
 
-    print('Closing and deleting file')
-    f.close()
-    os.remove('test_file.txt')
 
-
-def test_1_with_setup_and_teardown(setup_file):
+@pytest.mark.parametrize('updated_text,expected_result', [
+  (' More text', 'Some text More text'),
+  (' Different text', 'Some text Different text')
+])
+def test_with_setup_and_teardown(setup_file, updated_text, expected_result):
     text = setup_file.read()
     assert text == 'Some text'
 
-    setup_file.write(' More text')
+    setup_file.write(updated_text)
     setup_file.seek(0)  # needed for Windows specific bug
     new_text = setup_file.read()
-    assert new_text == 'Some text More text'
-
-
-def test_2_with_setup_and_teardown(setup_file):
-    text = setup_file.read()
-    assert text == 'Some text'
-
-    setup_file.write(' Different text')
-    setup_file.seek(0)  # needed for Windows specific bug
-    new_text = setup_file.read()
-    assert new_text == 'Some text Different text'
+    assert new_text == expected_result
